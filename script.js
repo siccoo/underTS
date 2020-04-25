46d2781775c97709ca35477f531331700e7e7811
siccoo <obichilaka@gmail.com>
2020-04-25 12:07:27 +0100

    updated script.ts

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function add(num1, num2) {
 button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 2;
+var number2 = 5.8;
+var result = add(number1, number2);
+console.log(result);
--- script.ts
+++ script.ts
@@ -10,3 +10,14 @@ button.addEventListener("click", function() {
     console.log(add(+input1.value, +input2.value));
 });
 
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+const number1 = 2;
+const number2 = 5.8;
+
+const result = add(number1, number2)
+console.log(result);
+
+
