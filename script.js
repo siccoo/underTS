4e0a44b7d0db93939f6cfac7098f5b1d4b5b0fdc
siccoo <obichilaka@gmail.com>
2020-05-03 08:16:46 +0100

    Updated script.ts

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-var ADMIN = 0;
-var READ_ONLY = 1;
-var AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-;
-var person = {
-    name: "Helen",
-    age: 25,
-    hobbies: ["Cooking", "Singing", "Praying"],
-    role: Role.ADMIN
-};
-// person.role.push('admin');
-// person.role[1] = 10;
-console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function add(n1, n2) {
+    return n1 + n2;
 }
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
+function printResult(num) {
+    console.log('Result ' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+printResult(add(5, 13));
+var combineValues;
+combineValues = add;
+console.log(combineValues(9, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
