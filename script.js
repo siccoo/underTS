3a6e06b2f7c32fceb0026da11ef74d6a4a3330f4
siccoo <obichilaka@gmail.com>
2020-05-03 09:00:31 +0100

    Updated script.ts

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 var userInput;
+var userName;
 userInput = 5;
 userInput = 'Bunny';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occured!!!', 500);
--- script.ts
+++ script.ts
@@ -1,4 +1,14 @@
 let userInput: unknown;
+let userName: string;
 
 userInput = 5;
 userInput = 'Bunny';
+if(typeof userInput === 'string') {
+    userName = userInput;
+}
+
+function generateError(message: string, code: number) {
+    throw { message: message, errorCode: code}
+}
+
+generateError('An error occured!!!', 500);
