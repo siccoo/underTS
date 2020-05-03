7f6e9db01d7ad5c88212eb2555cdc9789c8b7dab
siccoo <obichilaka@gmail.com>
2020-05-03 08:23:58 +0100

    Updated script.ts

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-function printResult(num) {
-    console.log('Result ' + num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 13));
-var combineValues;
-combineValues = add;
-console.log(combineValues(9, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var userInput;
+userInput = 5;
+userInput = 'Bunny';
--- script.ts
+++ script.ts
@@ -1,24 +1,4 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
-}
+let userInput: unknown;
 
-function printResult(num: number): void {
-    console.log('Result ' + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-    const result = n1 + n2;
-    cb(result);
-}
-
-printResult(add(5, 13));
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(9, 8));
-
-addAndHandle(10, 20, (result) => {
-    console.log(result); 
-});
+userInput = 5;
+userInput = 'Bunny';
